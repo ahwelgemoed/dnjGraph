8f18a0fcb4713fe9edbe3645846b7137ae778286
ahwelgemoed <arno.welgemoed@outlook.com>
2020-03-02 19:17:31 +0200

    chore(all): file Cleanup

--- client/components/AuthHelper.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
-
-export default class AuthHelper extends Component {
-  render() {
-    return this.props.children;
-  }
-}
--- client/components/CardComponents/CardPoem.js
+++ client/components/CardComponents/CardPoem.js
@@ -10,75 +10,109 @@ import {
 } from 'react-native-paper';
 import moment from 'moment';
 import Markdown from 'react-native-markdown-display';
-
+import { useMediaQuery } from 'react-responsive';
+import { observer } from 'mobx-react-lite';
+import { RootStoreContext } from '../../store/RootStore';
 const { width, height } = Dimensions.get('window');
 
-const CardPoem = ({ poem, navigation = null }) => {
-  // console.log(poem.date);
+const CardPoem = observer(
+  ({ poem, navigation = null, turnOffViewWitdht, view }) => {
+    const { poemsStore } = React.useContext(RootStoreContext);
+    const isDesktopOrLaptop = useMediaQuery({
+      query: '(min-device-width: 1224px)'
+    });
+    // console.log(poem.date);
+    // <View style={{ maxWidth: isDesktopOrLaptop ? 500 : '100%' }}>
+    // console.log(poem);
+    const whereToNavigate = () => {
+      if (view === 'ONE') {
+        console.log('ONE');
 
-  return (
-    <View style={{ width: width, borderRadius: 30 }}>
-      {/* http://localhost:4000/public/img/imgdisnetjy-6fa8df10-51b5-11ea-adbe-01c007ff9125.jpg */}
-      <Card
-        style={{
-          alignSelf: 'center',
-          width: width * 0.9,
-          marginTop: 10,
-          marginBottom: 10,
-          borderRadius: 20,
-          shadowColor: '#000',
-          shadowOffset: {
-            width: 0,
-            height: 3
-          },
-          shadowOpacity: 0.29,
-          shadowRadius: 4.65,
-
-          elevation: 7
-        }}
-        onPress={() =>
+        return (
           navigation &&
-          navigation.push('poem', {
-            poemId: poem.id
+          navigation.push('APoem', {
+            poemId: poem.id,
+            title: poem.title
           })
-        }
-      >
-        <Card.Cover
-          source={{
-            uri: `http://localhost:4000/public/img/${
-              poem.photoURL
-                ? poem.photoURL
-                : 'imgdisnetjy-6fa8df10-51b5-11ea-adbe-01c007ff9125.jpg'
-            }`
-          }}
-        />
-        <Card.Title
-          title={poem.title}
-          subtitle={`by: ${poem.handle}`}
-          titleStyle={{
-            fontFamily: 'raleway-boldI',
-            fontSize: 30,
-            opacity: 0.8
-          }}
-          // style={{
-          //   top: -190,
-          //   left: 0,
-          //   zIndex: 10
-          // }}
-          // left={props => <Avatar.Icon {...props} icon="folder" />}
-        />
-        <Card.Content>
-          <Markdown>{poem.bodyText}</Markdown>
-        </Card.Content>
-        <Card.Actions>
-          {poem.isDraft ? <Chip icon="information">Draft Mode</Chip> : null}
+        );
+      }
+      if (view === 'POST') {
+        console.log('POST');
+        poemsStore.fromDraftToEdit({ poem });
 
-          {/* <Button>Cancel</Button>
-          <Button>Ok</Button> */}
-        </Card.Actions>
-      </Card>
-    </View>
-  );
-};
+        return navigation && navigation.navigate('PostPoem');
+      }
+    };
+
+    return (
+      <View
+        style={{
+          width: isDesktopOrLaptop ? 500 : turnOffViewWitdht ? null : width,
+          borderRadius: 30
+        }}
+      >
+        {/* http://localhost:4000/public/img/imgdisnetjy-6fa8df10-51b5-11ea-adbe-01c007ff9125.jpg */}
+        <Card
+          style={{
+            alignSelf: 'center',
+            width: isDesktopOrLaptop ? 480 : width * 0.9,
+            marginTop: 10,
+            marginBottom: 10,
+            borderRadius: 20,
+            shadowColor: '#000',
+            shadowOffset: {
+              width: 0,
+              height: 3
+            },
+            shadowOpacity: 0.29,
+            shadowRadius: 4.65,
+            elevation: 7
+          }}
+          onPress={() => whereToNavigate()}
+        >
+          <Card.Cover
+            style={{ borderTopLeftRadius: 20, borderTopRightRadius: 20 }}
+            source={{
+              uri: `http://localhost:4000/public/img/${
+                poem.photoURL
+                  ? poem.photoURL
+                  : 'imgdisnetjy-6fa8df10-51b5-11ea-adbe-01c007ff9125.jpg'
+              }`
+            }}
+          />
+          {poem.isDraft ? (
+            <Chip
+              style={{
+                position: 'absolute',
+                top: 10,
+                left: 10,
+                backgroundColor: 'rgb(255, 121, 198)'
+              }}
+              icon="information"
+            >
+              Draft Mode
+            </Chip>
+          ) : null}
+          <Card.Title
+            title={poem.title}
+            subtitle={`by: ${poem.handle ? poem.handle : 'ANON'}`}
+            titleStyle={{
+              fontFamily: 'raleway-boldI',
+              fontSize: 20,
+              opacity: 0.8
+            }}
+          />
+          <Card.Content>
+            <Markdown>{poem.bodyText}</Markdown>
+          </Card.Content>
+          <Card.Actions>
+            <Button> {moment(poem.date).format(`MMM'YY`)}</Button>
+            <Button> {poem.date}</Button>
+          </Card.Actions>
+        </Card>
+      </View>
+    );
+  }
+);
 
 export default CardPoem;
