9e307d0edb608933ed29dbd361f52d454dd32e0c
ahwelgemoed <arno.welgemoed@outlook.com>
2020-11-06 16:55:19 +0100

    feat: :art: new DB

--- server/index.js
+++ server/index.js
@@ -1,29 +1,29 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const { ApolloServer, gql } = require('apollo-server-express');
-const mongoose = require('mongoose');
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const { ApolloServer, gql } = require("apollo-server-express");
+const mongoose = require("mongoose");
 
 const port = process.env.PORT || 4000;
-const restEndpoints = require('./rest/routes');
-const authHandler = require('./authHandler-service');
-const typeDefs = require('./schemas');
-const resolvers = require('./resolvers');
-
+const restEndpoints = require("./rest/routes");
+const authHandler = require("./authHandler-service");
+const typeDefs = require("./schemas");
+const resolvers = require("./resolvers");
+// mongodb+srv://<username>:<password>@disnetjygraph.qm7y4.mongodb.net/disnetjygraph?retryWrites=true&w=majority
 mongoose.connect(
-  `mongodb://${process.env.MLAB_USER}:${process.env.MLAB_PASSWORD}@ds026898.mlab.com:26898/disnetjygraph`,
+  `mongodb+srv://${process.env.MLAB_USER}:${process.env.MLAB_PASSWORD}@disnetjygraph.qm7y4.mongodb.net/disnetjygraph?retryWrites=true&w=majority`,
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
 const db = mongoose.connection;
 const app = express();
 app.use(cors());
-app.use(`/public`, express.static(path.join(__dirname, './public')));
-app.use('/v1', restEndpoints);
+app.use(`/public`, express.static(path.join(__dirname, "./public")));
+app.use("/v1", restEndpoints);
 
-db.on('error', console.error.bind(console, 'connection error:'));
+db.on("error", console.error.bind(console, "connection error:"));
 
-db.once('open', function () {
+db.once("open", function () {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
